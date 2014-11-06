24e266fcdf088c9d564b9da0bc6ccf403cc6e109
UsuallyGo <qinliok@126.com>
2014-11-06 17:19:57 +0800

    Modified 3259, but not AC yet.

--- 3259/3259.c
+++ 3259/3259.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <limits.h>
 
-#define DEBUG 1
+#define DEBUG 0
 
 typedef struct Path_t
 {
@@ -38,22 +38,41 @@ int Bellman_Ford(Path *p, int vertex, int edges)
     int first, second;
 
     dist = (int*)malloc(sizeof(int)*(vertex+1));
-    memset(dist, INT_MAX, sizeof(int)*vertex);
+    for(first=0; first<vertex+1; first++)
+	dist[first] = INT_MAX;
+#if DEBUG
+    printf("Bellman_ford vertex:%d edges:%d\n", vertex, edges);
+#endif
 
-    dist[1] = INT_MIN;
-   for(first=1; first<vertex-1; first++)
+    dist[1] = 0;
+    for(first=1; first<vertex; first++)
 	for(second=0; second<edges; second++)
 	{
+#if DEBUG
+	    printf("End dist[%d]:%d\n", p[second].end, dist[p[second].end]);
+	    printf("Source dist[%d]:%d\n", p[second].source, dist[p[second].source]);
+#endif
 	    if(dist[p[second].end] > (dist[p[second].source] + p[second].value))
 		dist[p[second].end] = dist[p[second].source] + p[second].value;
 	}
     
     for(first=0; first<edges; first++)
-	if(dist[p[first].end] > (dist[p[second].source] + p[second].value))
+    {	    	    
+#if DEBUG
+	printf("dist[%d]:%d  %d+%d\n", p[first].end, dist[p[first].end], dist[p[first].source], p[first].value);
+#endif
+	if(dist[p[first].end] > (dist[p[first].source] + p[first].value))
 	{
 	    free(dist);
 	    return 0;
 	}
+    }
+#if DEBUG
+    for(first=1; first<=vertex; first++)
+    {
+	printf("dist[%d]:%d\n", first, dist[first]);
+    }
+#endif
     
     free(dist);
     return 1;
@@ -92,14 +111,37 @@ int main()
 	pp = (Path*)malloc(sizeof(Path)*(fp.paths*2+fp.worms));
 	edges = 0;
 
-	for(first=0; first<fp.paths; first++, edges++)
-	    scanf("%d %d %d", &pp[edges].source, &pp[edges].end, &pp[edges].value); 
-	    
-	for(first=0; first<fp.paths; first++, edges++)
+	for(first=0; first<fp.paths; first++)
 	{
-	    pp[edges].source = pp[edges-fp.paths].end;
-	    pp[edges].end = pp[edges-fp.paths].source;
-	    pp[edges].value = pp[edges-fp.paths].value;
+	    scanf("%d %d %d", &pp[edges].source, &pp[edges].end, &pp[edges].value); 
+	    result = find(pp, edges, pp[edges].source, pp[edges].end);
+	    if(result >= 0)
+	    {
+#if DEBUG
+		printf("source:%d  end:%d value:%d\n", pp[edges].source, pp[edges].end, pp[edges].value);
+		printf("result source:%d end:%d value:%d\n", pp[result].source, pp[result].end, pp[result].value);
+#endif
+		if(pp[result].value > pp[edges].value)
+		{
+		    pp[result].value = pp[edges].value;
+		    result = find(pp, edges, pp[edges].end, pp[edges].source);
+		    pp[result].value = pp[edges].value;
+		}
+	    }
+	    else
+	    {
+		if(pp[edges].source != pp[edges].end)
+		{
+		    edges++;
+		    pp[edges].source = pp[edges-1].end;
+		    pp[edges].end = pp[edges-1].source;
+		    pp[edges].value = pp[edges-1].value;
+		}
+		edges++;
+	    }
+#if DEBUG
+	    printf("Pre pp[%d]  %d  %d  %d\n", edges-1, pp[edges-1].source, pp[edges-1].end, pp[edges-1].value);
+#endif
 	}
 
 	for(first=0; first<fp.worms; first++)
@@ -107,10 +149,7 @@ int main()
 	    scanf("%d %d %d", &pp[edges].source, &pp[edges].end, &pp[edges].value);
 	    result = find(pp, edges, pp[edges].source, pp[edges].end);
 	    if(result >= 0)
-	    {
 		pp[result].value = pp[edges].value*(-1);
-		continue;
-	    }
 	    else
 	    {
 		pp[edges].value = pp[edges].value*(-1);
@@ -122,6 +161,9 @@ int main()
 	show(pp, edges);
 #endif 
 	result = Bellman_Ford(pp, fp.fields, edges);
+#if DEBUG
+	printf("After Bellman result:%d\n", result);
+#endif
 	if(result)
 	    printf("NO\n");
 	else
